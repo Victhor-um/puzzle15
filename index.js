09e0c4ca37914187a7a3bfac2ab0ade9675eb375
Victor <uberman59@gmail.com>
2023-02-07 17:46:59 +0500

    feat: work replace

--- index.js
+++ index.js
@@ -40,3 +40,80 @@ function randomizeArray(arr) {
 }
 const restartButton = document.querySelector('#restart');
 restartButton.addEventListener('click', generateRandomGameField);
+
+gameField.onclick = function (event) {
+  const target = event.target.closest('td');
+  console.log('🚀 ~ file: index.js:47 ~ target INNER TEXT', target.innerText);
+  const trueNums = findEmptyElement(gameField);
+  if (trueNums.some((elem) => elem === target.innerText)) {
+    console.log('TRUE NUMS WORKS: HA HA');
+    wasteElement;
+    console.log('🚀 ~ file: index.js:53 ~ wasteElement', wasteElement);
+    wasteElement.innerText = target.innerText;
+    target.innerText = '';
+  }
+};
+let wasteElement;
+function findEmptyElement(gameField) {
+  const table = gameField.querySelector('table');
+  console.log('🚀 ~ file: index.js:52 ~ findEmptyElement ~ table', table);
+  console.log('table rows: ', table.rows);
+  const availableTDs = [];
+  const currentField = [];
+  const wastePosition = [];
+  for (let tr = 0; tr < table.rows.length; tr++) {
+    const line = [];
+    // console.log(
+    //   '🚀 ~ file: index.js:58 ~ findEmptyElement ~ tr',
+    //   table.rows[tr]
+    // );
+    for (let td = 0; td < table.rows[tr].cells.length; td++) {
+      //   console.log(
+      //     '🚀 ~ file: index.js:62 ~ findEmptyElement ~ td',
+      //     table.rows[tr].cells[td].innerText
+      //   );
+      //Нахождение пустого элемента
+      if (!table.rows[tr].cells[td].innerText) {
+        wasteElement = table.rows[tr].cells[td];
+        wastePosition.push(tr, td);
+      }
+      line.push(table.rows[tr].cells[td].innerText);
+    }
+    currentField.push(line);
+  }
+
+  console.log(
+    '🚀 ~ file: index.js:75 ~ findEmptyElement ~ wastePosition',
+    wastePosition
+  );
+  console.log(
+    '🚀 ~ file: index.js:69 ~ findEmptyElement ~ currentField',
+    currentField
+  );
+  //Заполняем доступные клетки для хода макс 4 мин 2
+
+  const temp = wastePosition[0] + 1;
+  console.log('🚀 ~ file: index.js:87 ~ findEmptyElement ~ temp', temp);
+
+  if (temp >= 0 && temp < 4) {
+    console.log('fist if: ');
+    availableTDs.push(currentField[temp][wastePosition[1]]);
+  }
+  const temp2 = wastePosition[0] - 1;
+  if (temp2 >= 0 && temp2 < 4) {
+    availableTDs.push(currentField[temp2][wastePosition[1]]);
+  }
+  const temp3 = wastePosition[1] + 1;
+  if (temp3 >= 0 && temp3 < 4) {
+    availableTDs.push(currentField[wastePosition[0]][temp3]);
+  }
+  const temp4 = wastePosition[1] - 1;
+  if (temp4 >= 0 && temp4 < 4) {
+    availableTDs.push(currentField[wastePosition[0]][temp4]);
+  }
+  console.log(
+    '🚀 ~ file: index.js:93 ~ findEmptyElement ~ availableTDs',
+    availableTDs
+  );
+  return availableTDs;
+}
